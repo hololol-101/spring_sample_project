87444bc07b71781de74f7719c786121b9fa4984c
KAWA <shrkddms1010@naver.com>
2022-05-18 00:34:22 +0900

    Frontend Pageable 구현

--- src/main/java/com/example/board/controller/boardController.java
+++ src/main/java/com/example/board/controller/boardController.java
@@ -3,6 +3,7 @@ package com.example.board.controller;
 import com.example.board.entity.Board;
 import com.example.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -36,9 +37,14 @@ public class boardController {
 
     @GetMapping("/board/list")
     public String boardList(Model model,@PageableDefault(page = 0, size=10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-
-        model.addAttribute("list", boardService.boardList(pageable));
-
+        Page<Board> list = boardService.boardList(pageable);
+        int nowPage = list.getPageable().getPageNumber() + 1; //가장 작은 페이지가 0임
+        int startPage = Math.max(nowPage-4, 1);
+        int endPage = Math.min(nowPage+5, list.getTotalPages());
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "boardlist";
     }
 
